ad3fb659f9621b8cad40b219fa24011505fe3e32
nkong008 <nkong2020@gmail.com>
2024-01-10 21:47:25 -0600

    created data text

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+let text = document.getElementById("text");
+console.log(text);
+
+function getData() {
+    let myDataRequest;
+
+    if (window,XMLHttpRequest) {
+        myDataRequest = new XMLHttpRequest();
+    }
+    else {
+        myDataRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    // myDataRequest.onload = function() {
+    //     text.innerHTML = this.responseText;
+    // }
+    
+
+    myDataRequest.onload = ()=> {
+        text.innerHTML = myDataRequest.responseText;
+    }
+
+    myDataRequest.open("GET", "data.txt", true);
+    myDataRequest.send();
+}
