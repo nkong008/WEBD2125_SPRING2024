0d40af3fad8e6fe23362eff1670a59a8060aa46e
nkong008 <nkong2020@gmail.com>
2024-02-22 15:39:26 -0600

    need to fix my scroll magic

--- week-6/js/app.js
+++ week-6/js/app.js
@@ -78,7 +78,161 @@ $(function () {
         }
     })
 
+    let blockquote = $("section#sticky > blockquote");
+    let sticky = $("section#sticky");
+    let span = $("section#sticky > blockquote > span"); //add more
+    let artshow1 = $("img#artshow1.artshow");
+    let artshow2 = $("img#artshow2.artshow");
+    let box2 = $("section#sticky > div.box");
+
+    function scrollMagicDemo() {
+        let tl2 = gsap.timeline({
+            onUpdate: upDatePercentage
+        });
+    
+        let tl3 = gsap.timeline();
+    
+        const controller = new ScrollMagic.Controller();
+    
+    
+        tl2.from(blockquote, 0.5, {
+            x:200, opacity: 0
+        })
+    
+        tl2.from (span, 1, {
+            width: 0
+        },"=-0.5")
+    
+        tl2.from(artshow1, 1, {
+            x:-200,
+            opacity:0
+        },"=-1")
+    
+        tl2.from(artshow2, 1, {
+            x:200,
+            opacity:0
+        },"=-0.7")
+    
+        tl3.from(box2, 1, {
+            opacity:0,
+            scale: 0
+        })
+    
+        tl3.to(box2, 0.5, {
+            left: "20%",
+            scale: 1.3,
+            borderColor:"white",
+            borderWidth:"12",
+            boxShadow:"1px 1px 0px 0px rgba(0, 0, 0, .9)"
+    
+        })
+    
+        const scene = new ScrollMagic.Scene({
+            triggerElement: ".stikcy",
+            triggerHook: "onLeave",
+            duration: "100%",
+        })
+        
+        .setPin(sticky)
+        .setTween(tl2)
+        .addTo(controller)
+    
+    
+        // progress is unique to gsap timeline gets and sets a progress 
+        function upDatePercentage() {
+            tl2.progress();
+            console.log(tl2.progress());
+    
+        }
+    
+        const scene2 = new ScrollMagic.Scene( {
+            triggerElement:blockquote
+        })
+    
+        .setTween(tl3)
+        .addTo(controller)
+    }
+
+    // let tl2 = gsap.timeline( {
+    //     onUpdate: upDatePercentage
+    // });
+
+    // let tl3 = gsap.timeline();
+
+    // const controller = new ScrollMagic.Controller();
+
+
+
+    // tl2.from(blockquote, 0.5, {
+    //     x:200, opacity: 0
+    // })
+
+    // tl2.from (span, 1, {
+    //     width: 0
+    // },"=-0.5")
+
+    // tl2.from(artshow1, 1, {
+    //     x:-200,
+    //     opacity:0
+    // },"=-1")
+
+    // tl2.from(artshow2, 1, {
+    //     x:200,
+    //     opacity:0
+    // },"=-0.7")
+
+    // tl3.from(box2, 1, {
+    //     opacity:0,
+    //     scale: 0
+    // })
+
+    // tl3.to(box2, 0.5, {
+    //     left: "20%",
+    //     scale: 1.3,
+    //     borderColor:"white",
+    //     borderWidth:"12",
+    //     boxShadow:"1px 1px 0px 0px rgba(0, 0, 0, .9)"
+
+    // })
+
+    // const scene = new ScrollMagic.Scene({
+    //     triggerElement: ".stikcy",
+    //     triggerHook: "onLeave",
+    //     duration: "100%",
+    // })
+    
+    // .setPin(sticky)
+    // .setTween(tl2)
+    // .addTo(controller)
+
+
+    // // progress is unique to gsap timeline gets and sets a progress 
+    // function upDatePercentage() {
+    //     tl2.progress();
+    //     console.log(tl2.progress());
+
+    // }
+
+    // const scene2 = new ScrollMagic.Scene( {
+    //     triggerElement:blockquote
+    // })
+
+    // .setTween(tl3)
+    // .addTo(controller)
+
+
     // jQuery ends here
+    // scroll trigger demo
+
+    let tl2 = gsap.timeline ({
+        scrollTrigger: {
+            trigger: sticky,
+            marker: true,
+            markers: {startColor: "red", endColor: "green", fontSize: "18px", fontWeight: "bold", indent: 20}
+        }
+    });
+
+    tl2.from(blockquote, {x: -200, duration: 1})
 
 });
 
